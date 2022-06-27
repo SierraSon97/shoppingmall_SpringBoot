8a0cd6e55ffd9f1bcfc46c4d80d65f2de6be06c5
SierraSon97 <swj1265@naver.com>
2022-06-27 22:40:57 +0900

    Initial commit

---
+++ src/main/java/com/example/shoppingmall_springboot/ShoppingmallSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmall_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallSpringBootApplication.class, args);
+    }
+
+}
